57984c085a1aed3d8982d27330982c1549b95343
Мария Веретенникова <veret.maria@gmail.com>
2022-03-22 00:17:42 +0300

    Homework_4 ver_1.2

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from .forms import (CreationForm, UserLoginForm, 
+from .forms import (CreationForm, UserLoginForm,
                     PasswordResetForm, UserLogoutForm)
 
 
